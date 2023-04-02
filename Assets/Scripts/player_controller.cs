860f81fe5419a752fb807344441875da8d753700
Rasmus <rasse.rask@gmail.com>
2023-04-02 12:41:33 +0300

    Input actions added in, now they need integration

---
+++ Assets/Scripts/player_controller.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_controller : MonoBehaviour
+{
+   /* private PlayerInputAction playerInputAction;
+    private Rigidbody2D unitBody;
+
+    private void Awake() {
+        unitBody = GetComponent<Rigidbody2D>();
+
+        PlayerInputAction playerInputAction = new PlayerInputAction();
+        playerInputAction.Player.Interact.performed += Interact;
+    }
+
+    public void Interact(InputAction.CallbackContext context){
+        //CODE FOR INTERACTING GOES HERE.
+        //https://www.youtube.com/watch?v=Yjee_e4fICc Check 17:19 for the error
+        //Player input module should no longer be needed.
+    }*/
+}
